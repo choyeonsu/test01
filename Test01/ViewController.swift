44af1f3f05b4740527fdc11d10e3ab851b3d44e3
조연수 <archflare@gmail.com>
2016-09-05 20:36:45 +0900

    프로젝트 추가

--- Test01/ViewController.swift
+++ Test01/ViewController.swift
@@ -1,25 +1,74 @@
-//
-//  ViewController.swift
-//  Test01
-//
-//  Created by 조연수 on 2016. 9. 3..
-//  Copyright © 2016년 조연수. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, UIWebViewDelegate
+{
+    @IBOutlet weak var textFieldURL: UITextField!
+    @IBOutlet weak var webViewMain: UIWebView!
+    @IBOutlet weak var activityIndicatorViewWaitingMark: UIActivityIndicatorView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        webViewMain.delegate = self
+        
+        loadWebPage("http://www.naver.com")
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func loadWebPage(url: String)
+    {
+        let urlObject = NSURL(string: url)
+        let request = NSURLRequest(URL: urlObject!)
+        webViewMain.loadRequest(request)
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView)
+    {
+        activityIndicatorViewWaitingMark.startAnimating()
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView)
+    {
+        activityIndicatorViewWaitingMark.stopAnimating()
+    }
+    
+    @IBAction func touchUpInsideButtonGo(sender: UIButton) {
+        loadWebPage(textFieldURL.text!)
+    }
+    
+    @IBAction func touchUpInsideButtonSite1(sender: UIButton) {
+        loadWebPage("https://www.google.co.kr/")
+    }
+    
+    @IBAction func touchUpInsideButtonSite2(sender: UIButton) {
+        loadWebPage("https://namu.wiki")
+    }
+    
+    @IBAction func touchUpInsideButtonHTML(sender: UIButton) {
+    }
+    
+    @IBAction func touchUpInsideButtonFile(sender: UIButton) {
+    }
+    
+    @IBAction func tabBarButtonStop(sender: UIBarButtonItem) {
+        webViewMain.stopLoading()
+    }
+    
+    @IBAction func tabBarButtonRefresh(sender: UIBarButtonItem) {
+        webViewMain.reload()
+    }
+    
+    @IBAction func tabBarButtonGoBack(sender: UIBarButtonItem) {
+        webViewMain.goBack()
+    }
+    
+    @IBAction func tabBarButtonGoForward(sender: UIBarButtonItem) {
+        webViewMain.goForward()
+    }
+    
 }
-
